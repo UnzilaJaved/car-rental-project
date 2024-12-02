559648890830d69cad9d6f472bbd178ad583c668
HASSAN\User <pc16811.hassankhan@gmail.com>
2024-12-02 10:56:15 +0500

    no message

--- src/adminpages/adminhome.js
+++ src/adminpages/adminhome.js
@@ -1,32 +1,24 @@
-// AdminDashboard.js
 import React, { useState } from 'react';
 import './adminhome.css';
 import Sidebar from './sidebaradmin';
 import Header from './adminheader';
 import UserTable from './usertable';
-import UserData from './userData';
 
 const AdminDashboard = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
-  // Filter users based on search term
+  // Handle search input change
   const handleSearch = (event) => {
     setSearchTerm(event.target.value.toLowerCase());
   };
-
-  const filteredData = UserData.filter(user =>
-    user.firstname.toLowerCase().includes(searchTerm) ||
-    user.lastname.toLowerCase().includes(searchTerm) ||
-    user.email.toLowerCase().includes(searchTerm) ||
-    user.phone.includes(searchTerm)
-  );
 
   return (
     <div className="dashboard-container">
       <Sidebar />
       <div className="main-content">
         <Header onSearch={handleSearch} />
-        <UserTable userData={filteredData} />
+        {/* Pass the searchTerm to UserTable */}
+        <UserTable searchTerm={searchTerm} />
       </div>
     </div>
   );
