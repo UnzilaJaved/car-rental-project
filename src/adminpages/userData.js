cbeb98c0a2e761eef3be47d06349b644c81ff10c
HASSAN\User <pc16811.hassankhan@gmail.com>
2024-12-04 19:38:37 +0500

    no message

--- src/adminpages/userData.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const UserData = [
-    { id: 2, firstname: 'abdelaziz', lastname: 'mrabet', email: 'mrabet@gmail.com', phone: '0695051538' },
-    { id: 8, firstname: 'monir', lastname: 'el hajjioui', email: 'promonir@gmail.com', phone: '0695051534' }
-  ];
-  export default UserData;
